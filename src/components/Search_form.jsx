227e64ab6ab0be5ea11118ba78e95614f5735cf9
worby82 <vorobiev.worby@yandex.ru>
2022-04-20 15:40:07 +0400

    react-pub test fake api

--- src/components/Search_form.jsx
+++ src/components/Search_form.jsx
@@ -5,7 +5,7 @@ import '../css/search.css'
 
 const Search_form = ({ searchValue, setSearchValue }) => {
     return (
-        <form className={search()} action="" method="get">
+        <form className={search()} action="" method="get" onSubmit={e => { e.preventDefault(); }}>
             <input className={search('input')} type="text" placeholder="Поиск" value={searchValue} onChange={setSearchValue} />
             <button className={btn({ 'search-form': true })} type="button">
                 <Icon cn={btn('icon', { search: true })} icon='search' />
